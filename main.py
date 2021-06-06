d60dc14ab75c36085c2048d1333cc46308850ea4
fidvrska <zuzia.fidurska@gmail.com>
2021-06-06 18:20:51 +0200

    powers_play

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-T = []
-for i in range(5):
-    n = int(input("--> "))
-    T.append(n)
+def potega(podstawa, wykladnik):
+    wynik = podstawa
+    for i in range(wykladnik - 1):
+        wynik = wynik * podstawa
+    return wynik
 
-x = int(input("index: "))
-print(T[x - 1])
+
+podstawa = int(input('Podaj podstawe: '))
+wykladnik = int(input('Podaj wykladnik: '))
+
+print('Wynik wynosi {}.'.format(potega(podstawa, wykladnik)))
