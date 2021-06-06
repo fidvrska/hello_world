32aefa4ca37e9f397efde399554e20abf4be263f
fidvrska <zuzia.fidurska@gmail.com>
2021-06-06 18:35:15 +0200

    pgzero introduction

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
-def potega(podstawa, wykladnik):
-    wynik = podstawa
-    for i in range(wykladnik - 1):
-        wynik = wynik * podstawa
-    return wynik
+import pgzrun
+WIDTH = 500
+HEIGHT = 500
+
+player = Rect((250, 250), (100, 100))
 
 
-podstawa = int(input('Podaj podstawe: '))
-wykladnik = int(input('Podaj wykladnik: '))
+def draw():
+    screen.fill('black')
+    screen.draw.filled_rect(player, 'red')
 
-print('Wynik wynosi {}.'.format(potega(podstawa, wykladnik)))
+
+def update():
+    if keyboard.right and player.x < 400:
+        player.x += 10
+    if keyboard.left and player.x > 0:
+        player.x -= 10
+    if keyboard.up:
+        player.y -= 10
+    if keyboard.down:
+        player.y += 10
+
+#
+# def on_key_down(key):
+#     if key == keyboard.RIGHT:
+#         print('!!')
+#         player.x += 10
+
+pgzrun.go()
